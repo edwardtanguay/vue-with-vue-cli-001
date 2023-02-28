5c5002de9df5907b2b99d678a732c251cc3efc52
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-28 15:26:04 +0100

    added component

---
+++ src/components/InfoSpace.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <p>
+      This is a new component with the message "{{message}}".
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoSpace',
+  props: {
+    message: String
+  }
+}
+</script>
+
+<style scoped>
+	p {
+		color: black;
+		text-shadow: .1rem .1rem .3rem orange;
+		font-size: 2rem;
+	}
+</style>
